cafc83fa93e0cc4ce0286204a7f6bf8b8c98bb71
scrazyakai <2145303367@qq.com>
2025-10-01 23:46:35 +0800

    init

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -6,8 +6,8 @@ export default defineUserConfig({
   base: "/blog/",
 
   lang: "zh-CN",
-  title: "博客演示",
-  description: "vuepress-theme-hope 的博客演示",
+  title: "青云小筑",
+  description: "青云小筑",
 
   theme,
 
