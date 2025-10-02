0064f97684375f2f57c3b3d5c2cb9c279b9bcd6a
scrazyakai <2145303367@qq.com>
2025-10-02 18:21:17 +0800

    更改title图标

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -8,8 +8,12 @@ export default defineUserConfig({
   lang: "zh-CN",
   title: "青云小筑",
   description: "青云小筑",
-
   theme,
+  head: [
+    ["link", { rel: "icon", type: "image/png", href: "/favicon.png" }],
+    // 如果用 PNG 或 SVG 图标，可以改成:
+    // ["link", { rel: "icon", type: "image/png", href: "/favicon.png" }],
+  ],
 
   // 和 PWA 一起启用
   // shouldPrefetch: false,
