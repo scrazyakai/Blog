c03d9b35a1a872e6a37ecdc9692ac216bca198e2
scrazyakai <2145303367@qq.com>
2025-10-02 16:02:55 +0800

    更新

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/52xinxin.top/blog/",
+  base: "/blog/",
 
   lang: "zh-CN",
   title: "青云小筑",
